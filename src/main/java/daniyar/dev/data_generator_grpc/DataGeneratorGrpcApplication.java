addc2f5d6754722cd1edc51ad7e33415f4e8a86a
Daniar <daniyaruteg123@gmail.com>
2024-07-16 14:47:04 +0500

    first commit

---
+++ src/main/java/daniyar/dev/data_generator_grpc/DataGeneratorGrpcApplication.java
@@ -0,0 +1,13 @@
+package daniyar.dev.data_generator_grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataGeneratorGrpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataGeneratorGrpcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/daniyar/dev/data_generator_grpc/DataGeneratorGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package daniyar.dev.data_generator_grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataGeneratorGrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
